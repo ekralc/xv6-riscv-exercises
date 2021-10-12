8ecc0e28dba0f7a922add837e3ac8ddc4025a9de
James <35628223+ekralc@users.noreply.github.com>
2021-10-12 04:29:03 +0100

    Simplify the repeated for loop

--- user/pingpong.c
+++ user/pingpong.c
@@ -2,30 +2,27 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
+#define MSG_SIZE 1
+
 int main(int argc, char *argv[]) {
   int a[2], b[2];
   pipe(a); pipe(b);
 
-  int pid;
-  char buf[1];
+  int recv = a[0], send = b[1];
 
-  pid = fork();
-
-  if (pid != 0) {
-    // get the ball rolling
-    write(a[1], "X", 1);
-
-    for (;;) {
-      read(b[0], buf, sizeof buf);
-      printf("process %d received %s\n", getpid(), buf);
-      write(a[1], buf, sizeof buf);
-    }
+  if (fork() == 0) {
+    // We're in the child process, so swap the recv and send pointers
+    recv = b[0], send = a[1];
   } else {
-    for (;;) {
-      read(a[0], buf, sizeof buf);
-      printf("process %d received %s\n", getpid(), buf);
-      write(b[1], buf, sizeof buf);
-    } 
+    // Initial message
+    write(send, "X", MSG_SIZE);
+  }
+
+  char buf[MSG_SIZE];
+  for (;;) {
+    read(recv, buf, sizeof buf);
+    printf("process %d received %s\n", getpid(), buf);
+    write(send, buf, sizeof buf);
   }
 
   exit(0);
